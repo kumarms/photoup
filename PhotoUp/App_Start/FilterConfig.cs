d67c95863b250e077d61f1c30af4333f0f2f4ac1
Kyle Fritz <kyle.p.fritz@gmail.com>
2013-06-06 01:57:57 -0400

    start

---
+++ PhotoUp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoUp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PhotoUp/Controllers/SessionController.cs
@@ -0,0 +1,34 @@
+﻿using AttributeRouting.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoUp.Controllers
+{
+    public class SessionController : Controller
+    {
+        [GET("/session")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [POST("/session")]
+        public ActionResult Create(string user)
+        {
+            Session["user"] = user;
+
+            return Redirect("/");
+        }
+
+        [POST("/session/destroy")]
+        public ActionResult Destroy()
+        {
+            Session.Abandon();
+
+            return Redirect("/");
+        }
+    }
+}
