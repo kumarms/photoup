0cd36cf876fe7679b3f2bbf54fe8ffef01650be4
Kyle Fritz <kyle.p.fritz@gmail.com>
2013-06-06 02:51:58 -0400

    photo upload to s3 & display

---
+++ PhotoUp/Controllers/PhotoController.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.S3;
+using AttributeRouting.Web.Mvc;
+using PhotoUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoUp.Controllers
+{
+    public class PhotoController : Controller
+    {
+        [GET("/")]
+        public ActionResult Index()
+        {
+            return View(model: Photo.Load());
+        }
+
+        [GET("/photo")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [POST("/photo")]
+        public ActionResult Create(Photo photo)
+        {
+            var photoFile = Request.Files["photo-file"];
+
+            photo.User = Session["user"] as string;
+            photo.PhotoUrl = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(photoFile.FileName);
+
+            using (var s3 = new AmazonS3Client())
+            {
+                s3.PutObject(new Amazon.S3.Model.PutObjectRequest
+                {
+                    CannedACL = Amazon.S3.Model.S3CannedACL.PublicRead,
+                    BucketName = "betamore-photoup",
+                    InputStream = photoFile.InputStream,
+                    Key = photo.PhotoUrl
+                });
+            }
+
+            Photo.Create(photo);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PhotoUp/Models/Photo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace PhotoUp.Models
+{
+    public class Photo
+    {
+        public string User { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public DateTime Created { get; set; }
+
+        public static void Create(Photo photo)
+        {
+            using (var db = Db())
+            {
+                db.Execute(@"
+                    INSERT INTO [Photos]
+                        ([user],   description,  photourl, created)
+                    values
+                        (@user, @description, @photourl, GETUTCDATE())
+                ", photo);
+            }
+        }
+
+        public static IEnumerable<Photo> Load()
+        {
+            using (var db = Db())
+            {
+                return db.Query<Photo>(@"
+                    SELECT * FROM [Photos]
+                    ORDER BY [Created] DESC
+                ");
+            }
+        }
+
+        private static DbConnection Db()
+        {
+            var setting = System.Configuration.ConfigurationManager.ConnectionStrings["photoup"];
+            var factory = DbProviderFactories.GetFactory(setting.ProviderName);
+
+            var conn = factory.CreateConnection();
+            conn.ConnectionString = setting.ConnectionString;
+            conn.Open();
+
+            return conn;
+        }
+    }
+}
